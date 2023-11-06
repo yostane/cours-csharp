281b850b5f47ab5a17ffe03121f38a0e44880e72
yostane <1958676+yostane@users.noreply.github.com>
2023-11-06 17:11:51 +0100

    Correction de quelques exos

--- material/dotnet/file_exo/Program.cs
+++ material/dotnet/file_exo/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿static void PrintCountPerLine(String filePath)
+{
+  using StreamReader sr = new(filePath);
+  String? line = sr.ReadLine();
+  while (line != null)
+  {
+    Console.WriteLine(line.Length);
+    line = sr.ReadLine();
+  }
+}
+
+static void AddDotIfNotPresent(String filePath)
+{
+  using StreamReader sr = new(filePath);
+  using StreamWriter sw = new("dotted_file.txt");
+  String? line = sr.ReadLine();
+  while (line != null)
+  {
+    if (!line.EndsWith('.'))
+    {
+      line += '.'; // autre façon: line = $"{line}.";
+    }
+    sw.WriteLine(line);
+    line = sr.ReadLine();
+  }
+}
+
+
+
+String filePath = "input_file.txt";
+PrintCountPerLine(filePath);
+AddDotIfNotPresent(filePath);
