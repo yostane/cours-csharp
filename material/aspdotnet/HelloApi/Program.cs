5efc30367b1cb69f87e505c739522f1799e7b383
yostane <1958676+yostane@users.noreply.github.com>
2024-10-21 16:55:45 +0200

    Update helloAPI

--- material/aspdotnet/HelloApi/Program.cs
+++ material/aspdotnet/HelloApi/Program.cs
@@ -11,3 +11,14 @@ app.MapGet("/heroes/{id}", (int id) => heroes[id]);
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+public enum Role
+{
+  Fighter, Mage, Spport, Shooter
+}
+public class Hero
+{
+  public int Id { get; set; }
+  public required string Name { get; set; }
+  public required Role Role { get; set; }
+}
